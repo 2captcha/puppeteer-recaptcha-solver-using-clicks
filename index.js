d32fbb1da45d8f31b2ccfbf91e67c7432f6b300b
dzmitry-duboyski <dzmitry.duboyski@gmail.com>
2024-11-12 17:49:39 +0400

    Refactor

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const { isFoundRecaptchaChallengeFrame } = require('./utils/isFoundRecaptchaChal
 const { captchaSolver } = require('./captchaSolver')
 const sleep = (time) => new Promise(resolve => setTimeout(resolve, time));
 
+// Target page with reCAPTCHA
+const targetPage = 'https://2captcha.com/demo/recaptcha-v2'
+
+// CSS selectors
+const recaptchaBadgeIframeSelector = 'iframe[title="reCAPTCHA"]'
+const recaptchaCheckboxSelector = 'span.recaptcha-checkbox-unchecked'
+const submitButtonSelector = '[data-action="demo_action"]' // The selector that is clicked after the successful solution of the captcha
+
 ;(async () => {
   const launchOptions = {
     headless: false, 
@@ -27,9 +35,9 @@ const sleep = (time) => new Promise(resolve => setTimeout(resolve, time));
   });
   await sleep(10000);
   // Go to the page containing the captcha
-  await page.goto('https://2captcha.com/demo/recaptcha-v2');
+  await page.goto(targetPage);
 
-  await page.waitForSelector('iframe[title="reCAPTCHA"]', { timeout: 30000 });
+  await page.waitForSelector(recaptchaBadgeIframeSelector, { timeout: 30000 });
   await sleep(5000);
 
   // Search for reCAPTCHA Badge on the page
@@ -39,15 +47,19 @@ const sleep = (time) => new Promise(resolve => setTimeout(resolve, time));
   if(isFoundReCaptcha) { 
     console.log(`reCAPTCHA Badge is found.`);
   } else {
-    throw new Error('Error reCAPTCHA Badge not found! ');
+    throw new Error('reCAPTCHA Badge not found!');
   }
 
-  // Getting a frame based on the content in the URL
-  const recaptchaBadgeFrame = page.frames().find(frame => frame.url().includes('https://www.google.com/recaptcha/api2'));
+  // Getting a recaptcha badge iframe
+  const iframeElementHandle = await page.$(recaptchaBadgeIframeSelector);
+  const recaptchaBadgeIframe = await iframeElementHandle.contentFrame();
 
-  if (recaptchaBadgeFrame) {
-    // Click on the button inside the frame (recaptcha checkbox)
-    await recaptchaBadgeFrame.click('span.recaptcha-checkbox-unchecked'); 
+  if (recaptchaBadgeIframe) {
+    // Click on checkbox reCAPTCHA
+    await recaptchaBadgeIframe.evaluate((recaptchaCheckboxSelector) => {
+      const recaptchaCheckbox = document.querySelector(recaptchaCheckboxSelector)
+      recaptchaCheckbox.click()
+    },recaptchaCheckboxSelector) 
     
     await sleep(5000); 
 
@@ -73,11 +85,11 @@ const sleep = (time) => new Promise(resolve => setTimeout(resolve, time));
     }
 
   } else {
-    console.log('reCAPTCHA badge not found!');
+    throw new Error('reCAPTCHA Badge frame not found!')
   }
 
   // Click on button to check rerult
-  await page.click('[data-action="demo_action"]')
+  await page.click(submitButtonSelector)
   await sleep(5000)
   await browser.close();
 })();
